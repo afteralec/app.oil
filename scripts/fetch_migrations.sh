e6aa722529a9211b41a7ff91ae11fd2a0fd783ed
Alec DuBois <after.alec@gmail.com>
2023-10-20 12:32:46 -0700

    Fix fetch_migrations script

--- scripts/fetch_migrations.sh
+++ scripts/fetch_migrations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl --location --remote-header-name --remote-name https://raw.githubusercontent.com/afteralec/db.oil/main/schema.hcl \
-  && atlas migrate diff --dev-url "mysql://root:pass@:3306/clean" --to "file://schema.hcl"
+  && atlas migrate diff --dev-url "mysql://root:pass@:3306/test" --to "file://schema.hcl"
