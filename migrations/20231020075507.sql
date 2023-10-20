4326e31013a332d13c7b2f6c01ca6d149d4767f8
Alec DuBois <after.alec@gmail.com>
2023-10-20 09:18:00 -0700

    Remove copies of schema and migrations from repo

--- migrations/20231020075507.sql
+++
@@ -1,6 +0,0 @@
--- Create "features" table
-CREATE TABLE `features` (`id` bigint NOT NULL AUTO_INCREMENT, `flag` varchar(255) NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `features_flag` (`flag`)) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
--- Create "players" table
-CREATE TABLE `players` (`id` bigint NOT NULL AUTO_INCREMENT, `username` varchar(16) NOT NULL, `pw_hash` varchar(255) NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `players_username` (`username`)) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
--- Create "requests" table
-CREATE TABLE `requests` (`id` bigint NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`)) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
