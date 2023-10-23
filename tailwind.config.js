6e3d1f525bf8a97794de6dd632e8bb7ebf27ccee
Alec DuBois <after.alec@gmail.com>
2023-10-23 15:15:35 -0700

    Start composing layout for requests

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./web/views/*.html"],
+  content: [
+    "./web/views/*.html",
+    "./web/views/partials/*.html",
+    "./web/views/layouts/*.html"
+  ],
   theme: {
     extend: {},
   },
