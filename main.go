a8bf7e8019981d9a7d6c4d7e3632a9e3300e8ef0
Alec DuBois <after.alec@gmail.com>
2023-10-25 02:45:55 -0700

    Add register modal

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"embed"
-  "log"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	fiber "github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-  
-  "petrichormud.com/app/internal/middleware"
+
 	"petrichormud.com/app/internal/configs"
 	"petrichormud.com/app/internal/handlers"
+	"petrichormud.com/app/internal/middleware"
 	"petrichormud.com/app/internal/queries"
 )
 
@@ -26,16 +26,16 @@ func main() {
 
 	app.Use(cors.New())
 	app.Use(logger.New())
-  app.Use(middleware.Session())
+	app.Use(middleware.Session())
 
 	app.Static("/", "./web/static")
 
 	app.Get("/", handlers.Home)
 
-  player := app.Group("player")
-  player.Post("/", handlers.NewPlayer)
+	player := app.Group("player")
+	player.Post("/", handlers.NewPlayer)
 
-  request := app.Group("request")
+	request := app.Group("request")
 	request.Get("/:id", handlers.Request)
 
 	log.Fatal(app.Listen(":8008"))
