f11c6c8af4d4cc4531e5c64e265884690542d8a1
Alec DuBois <after.alec@gmail.com>
2023-10-28 12:03:40 -0700

    Deny login on missing permission

--- internal/handlers/login/handler.go
+++ internal/handlers/login/handler.go
@@ -2,11 +2,14 @@ package login
 
 import (
 	"context"
+	"slices"
 
 	fiber "github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
+	redis "github.com/redis/go-redis/v9"
 
 	"petrichormud.com/app/internal/password"
+	"petrichormud.com/app/internal/permissions"
 	"petrichormud.com/app/internal/queries"
 )
 
@@ -15,7 +18,7 @@ type LoginInput struct {
 	Password string `form:"password"`
 }
 
-func New(s *session.Store, q *queries.Queries) fiber.Handler {
+func New(s *session.Store, q *queries.Queries, r *redis.Client) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		i := new(LoginInput)
 
@@ -41,13 +44,24 @@ func New(s *session.Store, q *queries.Queries) fiber.Handler {
 			return nil
 		}
 
+		pid := p.ID
+		perms, err := permissions.List(q, r, pid)
+		if err != nil {
+			c.Status(fiber.StatusUnauthorized)
+			return nil
+		}
+		if !slices.Contains(perms, permissions.Login) {
+			c.Status(fiber.StatusUnauthorized)
+			return nil
+		}
+
 		sess, err := s.Get(c)
 		if err != nil {
 			c.Status(fiber.StatusUnauthorized)
 			return nil
 		}
 
-		sess.Set("pid", p.ID)
+		sess.Set("pid", pid)
 		if err = sess.Save(); err != nil {
 			c.Status(fiber.StatusUnauthorized)
 			return nil
