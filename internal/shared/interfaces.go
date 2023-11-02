74846ad0f842dece6f6a453c8ce6351e20102afa
Alec DuBois <after.alec@gmail.com>
2023-11-02 11:18:12 -0700

    Encapsulate setting up the shared interfaces

--- internal/shared/interfaces.go
+++ internal/shared/interfaces.go
@@ -1,13 +1,39 @@
 package shared
 
 import (
+	"context"
 	"database/sql"
+	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2/middleware/session"
 	redis "github.com/redis/go-redis/v9"
 
+	"petrichormud.com/app/internal/configs"
 	"petrichormud.com/app/internal/queries"
 )
+
+func SetupInterfaces() Interfaces {
+	db, err := sql.Open("mysql", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("SET GLOBAL local_infile=true;")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	opts := configs.Redis()
+	r := redis.NewClient(&opts)
+
+	// TODO: Update this config to be more secure. Will depend on environment.
+	s := session.New()
+
+	i := InterfacesBuilder().Database(db).Redis(r).Sessions(s).Build()
+	i.Ping()
+	return i
+}
 
 type Interfaces struct {
 	Database *sql.DB
@@ -42,4 +68,18 @@ func (b *interfacesBuilder) Sessions(s *session.Store) *interfacesBuilder {
 
 func (b *interfacesBuilder) Build() Interfaces {
 	return b.Interfaces
+}
+
+func (i *Interfaces) Ping() {
+	if err := i.Database.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	if err := i.Redis.Ping(context.Background()).Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (i *Interfaces) Close() {
+	i.Database.Close()
+	i.Redis.Close()
 }
--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"embed"
 	"log"
 	"net/http"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	fiber "github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/session"
 	html "github.com/gofiber/template/html/v2"
-	redis "github.com/redis/go-redis/v9"
 
 	"petrichormud.com/app/internal/configs"
 	"petrichormud.com/app/internal/handlers"
@@ -27,39 +22,14 @@ import (
 var viewsfs embed.FS
 
 func main() {
-	db, err := sql.Open("mysql", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec("SET GLOBAL local_infile=true;")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	opts := configs.Redis()
-	r := redis.NewClient(&opts)
-	defer r.Close()
-
-	if err := r.Ping(context.Background()); err != nil {
-		log.Fatal(err)
-	}
-
-	// TODO: Update this config to be more secure. Will depend on environment.
-	s := session.New()
-
-	i := shared.InterfacesBuilder().Database(db).Redis(r).Sessions(s).Build()
+	i := shared.SetupInterfaces()
+	defer i.Close()
 
 	views := html.NewFileSystem(http.FS(viewsfs), ".html")
 	app := fiber.New(configs.Fiber(views))
 
 	app.Use(logger.New())
-	app.Use(csrf.New(configs.CSRF(s)))
+	app.Use(csrf.New(configs.CSRF(i.Sessions)))
 	app.Use(sessiondata.New(&i))
 	app.Use(bind.New())
 
