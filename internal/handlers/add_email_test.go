343828dc99cc4ba333f076624f25d618411d9d8c
Alec DuBois <after.alec@gmail.com>
2023-11-02 18:19:41 -0700

    Add email test for malformed input

--- internal/handlers/add_email_test.go
+++ internal/handlers/add_email_test.go
@@ -214,6 +214,61 @@ func TestAddEmailDBDisconnected(t *testing.T) {
 	require.Equal(t, fiber.StatusInternalServerError, res.StatusCode)
 }
 
+func TestAddEmailMalformedInput(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	SetupTestAddEmail(t, &i)
+
+	views := html.New("../..", ".html")
+	config := configs.Fiber(views)
+	app := fiber.New(config)
+
+	// TODO: Extract all of this setup to its own functions?
+	app.Use(sessiondata.New(&i))
+
+	app.Post(LoginRoute, Login(&i))
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(AddEmailRoute, AddEmail(&i))
+
+	body, contentType := RegisterTestFormData()
+
+	// TODO: Extract this test url to a constant?
+	url := fmt.Sprintf("http://petrichormud.com%s", RegisterRoute)
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", contentType)
+	_, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, contentType = LoginTestFormData()
+
+	url = fmt.Sprintf("http://petrichormud.com%s", LoginRoute)
+	req = httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", contentType)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cookies := res.Cookies()
+	sessionCookie := cookies[0]
+
+	body, contentType = AddEmailMalformedTestFormData()
+
+	url = fmt.Sprintf("http://petrichormud.com%s", AddEmailRoute)
+	req = httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", contentType)
+	req.AddCookie(sessionCookie)
+	res, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusBadRequest, res.StatusCode)
+}
+
 func SetupTestAddEmail(t *testing.T, i *shared.Interfaces) {
 	_, err := i.Database.Exec("DELETE FROM players WHERE username = 'testify';")
 	if err != nil {
@@ -223,6 +278,14 @@ func SetupTestAddEmail(t *testing.T, i *shared.Interfaces) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func AddEmailMalformedTestFormData() (io.Reader, string) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("notemail", "blahblahblah")
+	writer.Close()
+	return body, writer.FormDataContentType()
 }
 
 func AddEmailTestFormData(address string) (io.Reader, string) {
