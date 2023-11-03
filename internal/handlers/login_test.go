9e7c6eea20eee79662071aa450d8039eafe523f2
Alec DuBois <after.alec@gmail.com>
2023-11-02 19:44:55 -0700

    Add login with wrong pw test

--- internal/handlers/login_test.go
+++ internal/handlers/login_test.go
@@ -97,6 +97,45 @@ func TestLoginSuccess(t *testing.T) {
 	require.Equal(t, fiber.StatusOK, res.StatusCode)
 }
 
+func TestLoginWithWrongPassword(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	SetupTestLogin(&i, t)
+
+	views := html.New("../..", ".html")
+	config := configs.Fiber(views)
+	app := fiber.New(config)
+
+	app.Post(LoginRoute, Login(&i))
+	app.Post(RegisterRoute, Register(&i))
+
+	body, contentType := LoginTestFormData()
+
+	// TODO: Extract this test url to a constant?
+	url := fmt.Sprintf("http://petrichormud.com%s", RegisterRoute)
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", contentType)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusCreated, res.StatusCode)
+
+	body, contentType = LoginTestFormDataWithPW("wrongpassword")
+
+	url = fmt.Sprintf("http://petrichormud.com%s", LoginRoute)
+	req = httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", contentType)
+	res, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusUnauthorized, res.StatusCode)
+}
+
 func SetupTestLogin(i *shared.Interfaces, t *testing.T) {
 	_, err := i.Database.Exec("DELETE FROM players WHERE username = 'testify';")
 	if err != nil {
@@ -109,6 +148,15 @@ func LoginTestFormData() (io.Reader, string) {
 	writer := multipart.NewWriter(body)
 	writer.WriteField("username", "testify")
 	writer.WriteField("password", "T3sted_tested")
+	writer.Close()
+	return body, writer.FormDataContentType()
+}
+
+func LoginTestFormDataWithPW(pw string) (io.Reader, string) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("username", "testify")
+	writer.WriteField("password", pw)
 	writer.Close()
 	return body, writer.FormDataContentType()
 }
