64338b986b583268d6c06ad30685d4c07a7fbd2f
Alec DuBois <after.alec@gmail.com>
2023-11-04 04:05:06 -0700

    Split app setup out into package

---
+++ internal/setup/middleware.go
@@ -0,0 +1,19 @@
+package setup
+
+import (
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	"petrichormud.com/app/internal/configs"
+	"petrichormud.com/app/internal/middleware/bind"
+	"petrichormud.com/app/internal/middleware/sessiondata"
+	"petrichormud.com/app/internal/shared"
+)
+
+func Middleware(app *fiber.App, i *shared.Interfaces) {
+	app.Use(logger.New())
+	app.Use(csrf.New(configs.CSRF(i.Sessions)))
+	app.Use(sessiondata.New(i))
+	app.Use(bind.New())
+}
---
+++ internal/setup/static.go
@@ -0,0 +1,10 @@
+package setup
+
+import (
+	fiber "github.com/gofiber/fiber/v2"
+)
+
+func Static(app *fiber.App) {
+	app.Static("/", "./web/static")
+	app.Static("/loaders", "./web/svg/loaders")
+}
