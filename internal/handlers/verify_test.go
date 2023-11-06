d0391dd2e05dc396e15cbf8d615d69cbdea0f7b7
Alec DuBois <after.alec@gmail.com>
2023-11-06 14:05:29 -0800

    Add Verify test for missing token

--- internal/handlers/verify_test.go
+++ internal/handlers/verify_test.go
@@ -62,6 +62,35 @@ func TestVerifyUnauthorized(t *testing.T) {
 	require.Equal(t, fiber.StatusUnauthorized, res.StatusCode)
 }
 
+func TestVerifyNoToken(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(VerifyRoute, Verify(&i))
+
+	CallRegister(t, app, TestUsername, TestPassword)
+	res := CallLogin(t, app, TestUsername, TestPassword)
+	sessionCookie := res.Cookies()[0]
+
+	url := fmt.Sprintf("%s%s", shared.TestURL, VerifyRoute)
+	req := httptest.NewRequest(http.MethodPost, url, nil)
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusBadRequest, res.StatusCode)
+}
+
 func SetupTestVerify(t *testing.T, i *shared.Interfaces, u string, e string) {
 	query := fmt.Sprintf("DELETE FROM players WHERE username = '%s'", u)
 	_, err := i.Database.Exec(query)
