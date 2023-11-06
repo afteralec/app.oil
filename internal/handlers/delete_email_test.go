47aed9408bbcea36429f5906d02e0615f1fc8122
Alec DuBois <after.alec@gmail.com>
2023-11-06 10:27:47 -0800

    Add more delete email failure case

--- internal/handlers/delete_email_test.go
+++ internal/handlers/delete_email_test.go
@@ -192,6 +192,102 @@ func TestDeleteEmailUnowned(t *testing.T) {
 	require.Equal(t, fiber.StatusForbidden, res.StatusCode)
 }
 
+func TestDeleteEmailInvalidID(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(AddEmailRoute, AddEmail(&i))
+	app.Delete(EmailRoute, DeleteEmail(&i))
+
+	SetupTestDeleteEmail(t, &i, TestUsername, TestEmailAddress)
+
+	CallRegister(t, app, TestUsername, TestPassword)
+	res := CallLogin(t, app, TestUsername, TestPassword)
+	cookies := res.Cookies()
+	sessionCookie := cookies[0]
+
+	url := fmt.Sprintf("%s/player/email/%s", shared.TestURL, "invalid")
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.AddCookie(sessionCookie)
+
+	res, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusBadRequest, res.StatusCode)
+}
+
+func TestDeleteNonexistantEmail(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(AddEmailRoute, AddEmail(&i))
+	app.Delete(EmailRoute, DeleteEmail(&i))
+
+	SetupTestDeleteEmail(t, &i, TestUsername, TestEmailAddress)
+
+	CallRegister(t, app, TestUsername, TestPassword)
+	res := CallLogin(t, app, TestUsername, TestPassword)
+	cookies := res.Cookies()
+	sessionCookie := cookies[0]
+	req := AddEmailRequest(TestEmailAddress)
+	req.AddCookie(sessionCookie)
+	_, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p, err := i.Queries.GetPlayerByUsername(context.Background(), TestUsername)
+	if err != nil {
+		t.Fatal(err)
+	}
+	emails, err := i.Queries.ListEmails(context.Background(), p.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	email := emails[0]
+
+	// TODO: Turn this route into a generator
+	url := fmt.Sprintf("%s/player/email/%d", shared.TestURL, email.ID)
+	req, err = http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.AddCookie(sessionCookie)
+
+	_, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusNotFound, res.StatusCode)
+}
+
 func TestDeleteEmailSuccess(t *testing.T) {
 	i := shared.SetupInterfaces()
 	defer i.Close()
