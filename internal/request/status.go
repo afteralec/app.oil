1ba33a224dd21abfd4f652ab4d6e68925b50a1b1
Alec DuBois <after.alec@gmail.com>
2023-12-07 14:28:24 -0800

    Reorganize update character application query

---
+++ internal/request/status.go
@@ -0,0 +1,13 @@
+package request
+
+const (
+	StatusIncomplete = "Incomplete"
+	StatusReady      = "Ready"
+	StatusSubmitted  = "Submitted"
+	StatusInReview   = "InReview"
+	StatusApproved   = "Approved"
+	StatusReviewed   = "Reviewed"
+	StatusRejected   = "Rejected"
+	StatusArchived   = "Archived"
+	StatusCanceled   = "Canceled"
+)
--- queries/request.sql
+++ queries/request.sql
@@ -10,3 +10,10 @@ SELECT * FROM requests WHERE pid = ? AND type = 'CharacterApplication';
 -- name: CreateRequest :execresult
 INSERT INTO requests (type, pid) VALUES (?, ?);
 
+-- name: CountOpenRequests :one
+SELECT
+  COUNT(*)
+FROM
+  requests
+WHERE
+  pid = ? AND status != "Archived" AND status != "Canceled";
