fffad406538722dba2e677b9b8055306e08cea70
Alec DuBois <after.alec@gmail.com>
2023-12-08 20:01:31 -0800

    Add test cases for request comment operations

--- internal/handlers/request_test.go
+++ internal/handlers/request_test.go
@@ -21,6 +21,37 @@ import (
 	"petrichormud.com/app/internal/shared"
 )
 
+func TestCreateCommentUnauthorized(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, _ := CharacterApplicationRID(t, &i, app)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusUnauthorized, res.StatusCode)
+}
+
 func TestCreateCommentSuccess(t *testing.T) {
 	i := shared.SetupInterfaces()
 	defer i.Close()
@@ -51,6 +82,104 @@ func TestCreateCommentSuccess(t *testing.T) {
 	}
 
 	require.Equal(t, fiber.StatusCreated, res.StatusCode)
+}
+
+func TestCreateCommentNotFound(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, sessionCookie := CharacterApplicationRID(t, &i, app)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid+1)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusNotFound, res.StatusCode)
+}
+
+func TestCreateCommentFatal(t *testing.T) {
+	i := shared.SetupInterfaces()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, sessionCookie := CharacterApplicationRID(t, &i, app)
+
+	i.Close()
+
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid+1)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusInternalServerError, res.StatusCode)
+}
+
+func TestCreateFieldCommentUnauthorized(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, _ := CharacterApplicationRID(t, &i, app)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("field", "name")
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusUnauthorized, res.StatusCode)
 }
 
 func TestCreateFieldCommentSuccess(t *testing.T) {
@@ -86,6 +215,108 @@ func TestCreateFieldCommentSuccess(t *testing.T) {
 	require.Equal(t, fiber.StatusCreated, res.StatusCode)
 }
 
+func TestCreateFieldCommentNotFound(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, sessionCookie := CharacterApplicationRID(t, &i, app)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid+1)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("field", "name")
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusNotFound, res.StatusCode)
+}
+
+func TestCreateFieldCommentFatal(t *testing.T) {
+	i := shared.SetupInterfaces()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, sessionCookie := CharacterApplicationRID(t, &i, app)
+
+	i.Close()
+
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("field", "name")
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusInternalServerError, res.StatusCode)
+}
+
+func TestCreateReplyCommentUnauthorized(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, _ := CharacterApplicationRID(t, &i, app)
+	cid, _ := CreateTestComment(t, &i, app, rid)
+	strcid := strconv.FormatInt(cid, 10)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("cid", strcid)
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusUnauthorized, res.StatusCode)
+}
+
 func TestCreateReplyCommentSuccess(t *testing.T) {
 	i := shared.SetupInterfaces()
 	defer i.Close()
@@ -119,6 +350,113 @@ func TestCreateReplyCommentSuccess(t *testing.T) {
 	}
 
 	require.Equal(t, fiber.StatusCreated, res.StatusCode)
+}
+
+func TestCreateReplyCommentNotFound(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, _ := CharacterApplicationRID(t, &i, app)
+	cid, sessionCookie := CreateTestComment(t, &i, app, rid)
+	strcid := strconv.FormatInt(cid, 10)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid+1)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("cid", strcid)
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusNotFound, res.StatusCode)
+}
+
+func TestCreateReplyCommentCIDNotFound(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, _ := CharacterApplicationRID(t, &i, app)
+	cid, sessionCookie := CreateTestComment(t, &i, app, rid)
+	strcid := strconv.FormatInt(cid+1, 10)
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("cid", strcid)
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusNotFound, res.StatusCode)
+}
+
+func TestCreateReplyCommentFatal(t *testing.T) {
+	i := shared.SetupInterfaces()
+
+	views := html.New("../..", ".html")
+	app := fiber.New(configs.Fiber(views))
+	app.Use(session.New(&i))
+	app.Use(bind.New())
+
+	app.Post(RegisterRoute, Register(&i))
+	app.Post(LoginRoute, Login(&i))
+	app.Post(NewCharacterRoute, NewCharacterApplication(&i))
+	app.Post(NewRequestCommentRoute, CreateRequestComment(&i))
+
+	rid, _ := CharacterApplicationRID(t, &i, app)
+	cid, sessionCookie := CreateTestComment(t, &i, app, rid)
+	strcid := strconv.FormatInt(cid, 10)
+
+	i.Close()
+
+	// TODO: Get this in a generator
+	url := fmt.Sprintf("%s/request/%d/comments/new", shared.TestURL, rid+1)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("text", "Test comment.")
+	writer.WriteField("cid", strcid)
+	writer.Close()
+	req := httptest.NewRequest(http.MethodPost, url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.AddCookie(sessionCookie)
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusInternalServerError, res.StatusCode)
 }
 
 func SetupTestRequests(t *testing.T, i *shared.Interfaces, u string) {
