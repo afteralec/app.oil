9165f94e942504c4640808d70c4f52e18e1cd40a
Alec DuBois <after.alec@gmail.com>
2024-01-07 01:15:38 -0800

    Remove unused theming

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,14 +31,6 @@ module.exports = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
       },
       maxWidth: {
         "10xl": "100rem",
