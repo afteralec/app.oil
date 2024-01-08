e4a2c9ebda5da26a16ea6b2bcb815d469251cbd9
Alec DuBois <after.alec@gmail.com>
2024-01-07 22:30:05 -0800

    Add set theme tests

---
+++ internal/tests/set_theme_test.go
@@ -0,0 +1,54 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/require"
+
+	"petrichormud.com/app/internal/app"
+	"petrichormud.com/app/internal/configs"
+	"petrichormud.com/app/internal/constants"
+	"petrichormud.com/app/internal/routes"
+	"petrichormud.com/app/internal/shared"
+)
+
+func TestSetThemeInvalid(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	a := fiber.New(configs.Fiber())
+	app.Middleware(a, &i)
+	app.Handlers(a, &i)
+
+	url := MakeTestURL(routes.ThemePath("notatheme"))
+	req := httptest.NewRequest(http.MethodPost, url, nil)
+
+	res, err := a.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusBadRequest, res.StatusCode)
+}
+
+func TestSetThemeSuccess(t *testing.T) {
+	i := shared.SetupInterfaces()
+	defer i.Close()
+
+	a := fiber.New(configs.Fiber())
+	app.Middleware(a, &i)
+	app.Handlers(a, &i)
+
+	url := MakeTestURL(routes.ThemePath(constants.ThemeLight))
+	req := httptest.NewRequest(http.MethodPost, url, nil)
+
+	res, err := a.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, fiber.StatusOK, res.StatusCode)
+}
