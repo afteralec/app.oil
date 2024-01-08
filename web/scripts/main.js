8fc02e3335818919c36d98b981ddb0131046ccb9
Alec DuBois <after.alec@gmail.com>
2024-01-07 17:00:18 -0800

    Prototype theme toggle

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,5 +1,15 @@
 "use strict";
 
+export function toggleTheme() {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  } else if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  }
+}
+
 export function getCSRFToken() {
   return document.querySelector('meta[name="csrf_"]').content;
 }
@@ -425,6 +435,7 @@ document.body.addEventListener("htmx:beforeOnLoad", (event) => {
   }
 });
 
+window.toggleTheme = toggleTheme;
 window.getCSRFToken = getCSRFToken;
 window.getRegisterData = getRegisterData;
 window.getResetPasswordData = getResetPasswordData;
