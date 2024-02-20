f7b01511333da4d54b216657417c140af67e56ac
Alec DuBois <after.alec@gmail.com>
2024-02-20 08:50:53 -0800

    Move more actor and player files to a non-suffixed version

--- internal/route/player.go
+++ internal/route/player.go
@@ -7,7 +7,8 @@ import (
 
 const (
 	Players                string = "/players"
-	PlayerPasswordParam    string = "players/:id/password"
+	PlayerPermissions      string = "/players/permissions"
+	PlayerPasswordParam    string = "/players/:id/password"
 	Login                  string = "/login"
 	Logout                 string = "/logout"
 	Register               string = "/player/new"
@@ -31,4 +32,20 @@ func PlayerPasswordPath(pid int64) string {
 	var sb strings.Builder
 	fmt.Fprintf(&sb, "%s/%d/password", Players, pid)
 	return sb.String()
+}
+
+func PlayerPermissionsSearchPath() string {
+	return fmt.Sprintf("%s/search", PlayerPermissions)
+}
+
+func PlayerPermissionsDetailPath(u string) string {
+	return fmt.Sprintf("%s/%s", PlayerPermissions, u)
+}
+
+func PlayerPermissionsPath(id string) string {
+	return fmt.Sprintf("%s/%s", PlayerPermissions, id)
+}
+
+func PlayerPermissionsTogglePath(id, tag string) string {
+	return fmt.Sprintf("%s/%s/%s", PlayerPermissions, id, tag)
 }
