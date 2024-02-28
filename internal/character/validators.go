c47f5126c7dd1d4fe20f841b4139c53817953494
Alec DuBois <after.alec@gmail.com>
2024-02-28 12:45:55 -0800

    Remove Unused Validators From Character Package

--- internal/character/validators.go
+++ internal/character/validators.go
@@ -2,8 +2,6 @@ package character
 
 import (
 	"regexp"
-
-	"petrichormud.com/app/internal/constant"
 )
 
 func SanitizeName(n string) string {
@@ -11,18 +9,6 @@ func SanitizeName(n string) string {
 	re := regexp.MustCompile("[^a-zA-Z'-]+")
 	s := re.ReplaceAllString(n, "")
 	return s
-}
-
-func IsNameValid(n string) bool {
-	if len(n) < constant.MinCharacterNameLength {
-		return false
-	}
-
-	if len(n) > constant.MaxCharacterNameLength {
-		return false
-	}
-
-	return true
 }
 
 const (
@@ -38,39 +24,11 @@ func SanitizeGender(str string) string {
 	return str
 }
 
-func IsGenderValid(gender string) bool {
-	if gender == GenderNonBinary {
-		return true
-	}
-
-	if gender == GenderFemale {
-		return true
-	}
-
-	if gender == GenderMale {
-		return true
-	}
-
-	return false
-}
-
 func SanitizeShortDescription(sdesc string) string {
 	// TODO: Add this regex test to IsShortDescriptionValid too
 	re := regexp.MustCompile("[^a-zA-Z, -]+")
 	s := re.ReplaceAllString(sdesc, "")
 	return s
-}
-
-func IsShortDescriptionValid(sdesc string) bool {
-	if len(sdesc) < constant.MinCharacterShortDescriptionLength {
-		return false
-	}
-
-	if len(sdesc) > constant.MaxCharacterShortDescriptionLength {
-		return false
-	}
-
-	return true
 }
 
 func SanitizeDescription(description string) string {
@@ -80,33 +38,9 @@ func SanitizeDescription(description string) string {
 	return s
 }
 
-func IsDescriptionValid(description string) bool {
-	if len(description) < constant.MinCharacterDescriptionLength {
-		return false
-	}
-
-	if len(description) > constant.MaxCharacterDescriptionLength {
-		return false
-	}
-
-	return true
-}
-
 func SanitizeBackstory(backstory string) string {
 	// TODO: Add this regex test to IsBackstoryValid too
 	re := regexp.MustCompile("[^\r\na-zA-Z, -.!()]+")
 	s := re.ReplaceAllString(backstory, "")
 	return s
-}
-
-func IsBackstoryValid(backstory string) bool {
-	if len(backstory) < constant.MinCharacterBackstoryLength {
-		return false
-	}
-
-	if len(backstory) > constant.MaxCharacterBackstoryLength {
-		return false
-	}
-
-	return true
 }
