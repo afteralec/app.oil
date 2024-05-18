93a77250e7d05fe3b390e7e4267f5ef80209e900
Alec DuBois <after.alec@gmail.com>
2024-05-18 09:58:38 -0700

    Add Tests for Change Request Text Validator
    Adds basic tests for Change Request text validator.

--- internal/request/change/validate_test.go
+++ internal/request/change/validate_test.go
@@ -16,3 +16,13 @@ func TestSanitizeTextWithInvalidChars(t *testing.T) {
 	expected := "test"
 	require.Equal(t, expected, SanitizeText(text))
 }
+
+func TestIsTextValidWithValid(t *testing.T) {
+	text := "Sorry, you can't use \"guy\" as a primary noun."
+	require.True(t, IsTextValid(text))
+}
+
+func TestIsTextValidWithInvalid(t *testing.T) {
+	text := "test1234@%%"
+	require.False(t, IsTextValid(text))
+}
